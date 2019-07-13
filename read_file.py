19ef6fc805292d146d60b2459d8ddb150a0e3a97
esaasstack <esaasstack@gmail.com>
2019-07-12 23:53:07 -0400

    added a python file

---
+++ read_file.py
@@ -0,0 +1,21 @@
+import os
+import argparse
+
+arguments = []
+parser = argparse.ArgumentParser()
+# parser.add_argument("echo", help="This will print the first variable")
+parser.add_argument("x", type=int, help="base")
+parser.add_argument("y", type=int, help="the exponent")
+parser.add_argument("-v","--verbose", help="Adding the verbosity", action="count", default=0)
+
+args = parser.parse_args()
+
+answer = args.x**args.y
+
+if args.verbose >= 2:
+    print("{} to the power {} equals {}".format(args.x,args.y,answer))
+elif args.verbose >= 1:
+    print("{}^{} == {}".format(args.x,args.y,answer))
+else:
+    print(answer)
+
