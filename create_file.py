bfb2953d6c4f93441d7b765575c28b5125d35a53
esaasstack <esaasstack@gmail.com>
2019-07-14 17:10:24 -0400

    added a file that creates roles and role mappings

--- create_file.py
+++ create_file.py
@@ -1,10 +1,34 @@
-import json
+import os
 import argparse
 
-content = "hey"
+#content in the file to be written
+roles = ['kibana','logrdr','report','monitor']
 
-def write_file():
-    with open('./{}_to_write.json'.format(content),'w+') as f:
-        f.write(content)
+def write_file(value):
+    for i in roles:
+        if not os.path.exists('./roles/'):
+            os.makedirs('./roles/')
+        elif os.path.exists('./roles/'):
+            with open('./roles/{}_{}_user.json'.format(value, i), 'w+') as f:
+                f.write('{}_{}_user'.format(value, i))
+        else:
+            with open('./roles/{}_{}_user.json'.format(value, i), 'w+') as f:
+                f.write('{}_{}_user'.format(value, i))
 
-write_file()
+        if not os.path.exists('./role_mappings/'):
+            os.makedirs('./role_mappings/')
+        elif os.path.exists('./role_mappings/'):
+            with open('./role_mappings/{}_{}_user.json'.format(value, i), 'w+') as g:
+                g.write('{}_{}_user'.format(value, i))
+        else:
+            with open('./role_mappings/{}_{}_user.json'.format(value, i), 'w+') as g:
+                g.write('{}_{}_user'.format(value, i))
+
+
+def commands():
+    arguments = argparse.ArgumentParser()
+    arguments.add_argument('n', type=str, help='name of the app to be created')
+    val = arguments.parse_args()
+    write_file(val.n)
+
+commands()
