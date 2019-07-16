7d936b6fb58fcb258bbef3fd82b0a583bebe4b2f
esaasstack <esaasstack@gmail.com>
2019-07-15 23:59:23 -0400

    modified python script

--- create_file.py
+++ create_file.py
@@ -1,34 +1,31 @@
 import os
 import argparse
 
-#content in the file to be written
-roles = ['kibana','logrdr','report','monitor']
 
 def write_file(value):
-    for i in roles:
-        if not os.path.exists('./roles/'):
-            os.makedirs('./roles/')
-        elif os.path.exists('./roles/'):
-            with open('./roles/{}_{}_user.json'.format(value, i), 'w+') as f:
-                f.write('{}_{}_user'.format(value, i))
-        else:
-            with open('./roles/{}_{}_user.json'.format(value, i), 'w+') as f:
-                f.write('{}_{}_user'.format(value, i))
-
-        if not os.path.exists('./role_mappings/'):
-            os.makedirs('./role_mappings/')
-        elif os.path.exists('./role_mappings/'):
-            with open('./role_mappings/{}_{}_user.json'.format(value, i), 'w+') as g:
-                g.write('{}_{}_user'.format(value, i))
-        else:
-            with open('./role_mappings/{}_{}_user.json'.format(value, i), 'w+') as g:
-                g.write('{}_{}_user'.format(value, i))
+    path = ['./roles/', './role_mappings/']
+    role_names = ['kibana','logrdr','monitoring','reporting']
+    for i in path:
+        if not os.path.exists(i):
+            os.makedirs(i)
+    for j in role_names:
+        with open('./roles/{}_{}_user.json'.format(value, j), 'w+') as f:
+            f.write('{}_{}_user'.format(value, i))
+        with open('./role_mappings/{}_{}_user.json'.format(value, j), 'w+') as g:
+            g.write('{}_{}_user'.format(value, i))
 
 
-def commands():
-    arguments = argparse.ArgumentParser()
-    arguments.add_argument('n', type=str, help='name of the app to be created')
-    val = arguments.parse_args()
-    write_file(val.n)
 
-commands()
+# def commands():
+#     arguments = argparse.ArgumentParser()
+#     arguments.add_argument('n', type=str, help='name of the app to be created')
+#     val = arguments.parse_args()
+#     write_file(val.n)
+
+if __name__=="__main__":
+    # content in the file to be written
+    with open('roles.yml', 'r+') as i:
+        roles = i.readlines()
+        for i in roles:
+            write_file(i.split('\n')[0])
+            commands.val
